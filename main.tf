7c9d493236f8abdaa0570a8956fc183877806a7c
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-28 17:19:17 -0500

    splat

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "transit" {
 
 # Create TGW subnets, attachments, and TGW Connect routes.
 resource "aws_subnet" "this" {
-  for_each = { for i in range(0, 1):
+  for_each = { for i in range(0, 1) :
     "${var.region_name_prefix}-tgw-${i + 1}" => {
       cidr_block = cidrsubnet(local.transit_cidr, 5, 14 + i)
       az         = distinct([for v in module.transit.vpc.subnets : regex("[a-z]{2}-[a-z]*-[0-9][a-z]", v.name)])[i]
@@ -30,7 +30,7 @@ resource "aws_subnet" "this" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  subnet_ids         = aws_subnet.this.*.id
-  vpc_id             = module.transit.vpc.vpc_id-00
+  subnet_ids         = aws_subnet.this[*].id
+  vpc_id             = module.transit.vpc.vpc_id
   transit_gateway_id = var.tgw_id
 }
